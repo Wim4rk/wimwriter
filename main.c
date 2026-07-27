0d6e0df7106a62d5457ce312a3cc81eedba7bff4
Wim4rk <olov.wimark@gmail.com>
2026-07-27 10:57:03 +0200

    Spell checking

--- main.c
+++ main.c
@@ -58,77 +58,76 @@ int main() {
 
         int ret = poll(fds, 1, prompt_timeout_ms);
         if (ret == 0) {
-                // TIMEOUT: 500 ms av inaktivitet har passerat
-                if (!prompt_visible) {
-                    int px = get_physical_x(cursor_col);
-                    int py = get_physical_y(cursor_row);
+            // TIMEOUT: 500 ms av inaktivitet har passerat
+            if (!prompt_visible) {
+                int px = get_physical_x(cursor_col);
+                int py = get_physical_y(cursor_row);
 
-                    // Rita ut understrecket i snabba A2-läget[cite: 1].
-                    render_char('_', px, py, target_addr);
-                    prompt_visible = true;
-                }
-            } else if (ret > 0) {
-                // HÄNDELSE: Data finns att läsa från tangentbordet
-                if (fds[0].revents & POLLIN) {
+                // Rita ut understrecket i snabba A2-läget[cite: 1].
+                render_char('_', px, py, target_addr);
+                prompt_visible = true;
+            }
+        } else if (ret > 0) {
+            // HÄNDELSE: Data finns att läsa från tangentbordet
+            if (fds[0].revents & POLLIN) {
 
-                    if (read(kb_fd, &ev, sizeof(ev)) > 0) {
-                        if (ev.type == EV_KEY && (ev.value == 1 || ev.value == 2)) {
+                if (read(kb_fd, &ev, sizeof(ev)) > 0) {
+                    if (ev.type == EV_KEY && (ev.value == 1 || ev.value == 2)) {
 
-                            // Städa bort prompten om den är synlig innan nästa tecken hanteras
-                            if (prompt_visible) {
-                                int px = get_physical_x(cursor_col);
-                                int py = get_physical_y(cursor_row);
+                        // Städa bort prompten om den är synlig innan nästa tecken hanteras
+                        if (prompt_visible) {
+                            int px = get_physical_x(cursor_col);
+                            int py = get_physical_y(cursor_row);
 
-                                render_char(' ', px, py, target_addr);
-                                prompt_visible = false;
-                            }
+                            render_char(' ', px, py, target_addr);
+                            prompt_visible = false;
+                        }
 
-                            // Hämta tecken från tabell
-                            char c = keyboard_get_char(&ev);
+                        // Hämta tecken från tabell
+                        char c = keyboard_get_char(&ev);
 
-                            switch (c) {
-                                case '\n': // Enter
-                                    cursor_col = 0;
-                                    cursor_row++;
-                                    break;
+                        switch (c) {
+                            case '\n': // Enter
+                                cursor_col = 0;
+                                cursor_row++;
+                                break;
 
-                                case 127: // Backspace
-                                cursor_col--;
-                                    if (cursor_col < 0) {
-                                        if (cursor_row > 0) {
-                                            cursor_row--;
-                                            cursor_col = MAX_COLS - 1;
-                                        } else {
-                                            cursor_col = 0;
-                                        }
+                            case 127: // Backspace
+                            cursor_col--;
+                                if (cursor_col < 0) {
+                                    if (cursor_row > 0) {
+                                        cursor_row--;
+                                        cursor_col = MAX_COLS - 1;
+                                    } else {
+                                        cursor_col = 0;
                                     }
-                                    // Overwrite buffer and screen with ' '
-                                    text_buffer[cursor_row][cursor_col] = ' ';
-                                    render_char(' ', get_physical_x(cursor_col), get_physical_y(cursor_row), target_addr);
-                                    break;
+                                }
+                                // Overwrite buffer and screen with ' '
+                                text_buffer[cursor_row][cursor_col] = ' ';
+                                render_char(' ', get_physical_x(cursor_col), get_physical_y(cursor_row), target_addr);
+                                break;
 
-                                default:
-                                    if (c > 0) { // Regular chars
-                                        int px = get_physical_x(cursor_col);
-                                        int py = get_physical_y(cursor_row);
+                            default:
+                                if (c > 0) { // Regular chars
+                                    int px = get_physical_x(cursor_col);
+                                    int py = get_physical_y(cursor_row);
 
-                                        text_buffer[cursor_row][cursor_col] = c;
-                                        render_char(c, px, py, target_addr);
+                                    text_buffer[cursor_row][cursor_col] = c;
+                                    render_char(c, px, py, target_addr);
 
-                                        cursor_col++;
+                                    cursor_col++;
 
-                                        // Word wrapping
-                                        if (cursor_col >= MAX_COLS) {
-                                            word_wrap(text_buffer, &cursor_row, &cursor_col, target_addr);
-                                        }
+                                    // Word wrapping
+                                    if (cursor_col >= MAX_COLS) {
+                                        word_wrap(text_buffer, &cursor_row, &cursor_col, target_addr);
                                     }
-                                    break;
-                            }
+                                }
+                                break;
+                        }
 
-                            // Jump mechanism
-                            if (cursor_row >= MAX_ROWS) {
-                                display_jump(text_buffer, &cursor_row, &cursor_col, target_addr);
-                            }
+                        // Jump mechanism
+                        if (cursor_row >= MAX_ROWS) {
+                            display_jump(text_buffer, &cursor_row, &cursor_col, target_addr);
                         }
                     }
                 }
