fa378e5bf93a23dc2e42bf762e3e15df5cad6ef7
Olov Wimark <olov.wimark@gmail.com>
2026-07-28 15:43:24 +0200

    Rotation all messed up - resetting

--- firmware/keyboard.h
+++ firmware/keyboard.h
@@ -11,4 +11,7 @@ void keyboard_close(int fd);
 // Tolka eventet och returnera tecknet (returnerar 0 om ogiltigt/osynligt)
 char keyboard_get_char(struct input_event *ev);
 
+bool keyboard_is_ctrl_pressed(void);
+bool keyboard_is_shift_pressed(void);
+
 #endif
