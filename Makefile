99669e7bef614fe351c0712a7fc932bd9176cf77
Wim4rk <olov.wimark@gmail.com>
2026-07-31 20:26:44 +0200

    Hopefully end of font trouble

--- Makefile
+++ Makefile
@@ -26,9 +26,8 @@ INCLUDES = -I./firmware \
 
 # Alla C-källkodsfiler
 SRCS = main.c \
-       fonts/wim_font_16x28.c \
        fonts/wim_font_24x32.c \
-       fonts/wim_font_24x41.c \
+       fonts/wim_font_24x43.c \
        firmware/display.c \
        firmware/fast_spi.c \
        firmware/keyboard.c \
--- fonts/wim_fonts.h
+++ fonts/wim_fonts.h
@@ -4,25 +4,21 @@
 #include <stdint.h>
 
 /* Dimensioner och bytestorlekar för statisk allokering */
-#define FONT_16X28_W 16
-#define FONT_16X28_H 28
-#define FONT_16X28_BYTES 448
 
 #define FONT_24X32_W 24
 #define FONT_24X32_H 32
 #define FONT_24X32_BYTES 768
 
 #define FONT_24X41_W 24
-#define FONT_24X41_H 41
+#define FONT_24X41_H 43
 #define FONT_24X41_BYTES 984
 
 /*
  * Typsnittsdatan deklareras externt här.
- * Arrayerna instansieras sedan i wim_fonts.c för att laddas direkt i RAM
+ * Arrayerna instansieras sedan i wim_fonts.h för att laddas direkt i RAM
  * vid uppstart av maskinen.
  */
-extern const uint8_t wim_font_16x28[128][FONT_16X28_BYTES];
 extern const uint8_t wim_font_24x32[256][FONT_24X32_BYTES];
-extern const uint8_t wim_font_24x41[128][FONT_24X41_BYTES];
+extern const uint8_t wim_font_24x41[128][FONT_24X43_BYTES];
 
 #endif // WIM_FONTS_H
