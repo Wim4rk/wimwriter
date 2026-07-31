939ecb4355bfe8d6da7dee8886945cbf7ea953eb
Wim4rk <olov.wimark@gmail.com>
2026-07-31 20:45:21 +0200

    Font everything!

--- fonts/wim_fonts.h
+++ fonts/wim_fonts.h
@@ -9,9 +9,9 @@
 #define FONT_24X32_H 32
 #define FONT_24X32_BYTES 768
 
-#define FONT_24X41_W 24
-#define FONT_24X41_H 43
-#define FONT_24X41_BYTES 984
+#define FONT_24X43_W 24
+#define FONT_24X43_H 43
+#define FONT_24X43_BYTES 984
 
 /*
  * Typsnittsdatan deklareras externt här.
@@ -19,6 +19,6 @@
  * vid uppstart av maskinen.
  */
 extern const uint8_t wim_font_24x43[256][FONT_24X32_BYTES];
-extern const uint8_t wim_font_24x41[128][FONT_24X43_BYTES];
+extern const uint8_t wim_font_24x32[256][FONT_24X43_BYTES];
 
 #endif // WIM_FONTS_H
